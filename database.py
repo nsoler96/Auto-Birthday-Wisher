d71ac1445fdc890faf73a298f55e00db6a7a22cc
nsoler96 <niobisoler96@gmail.com>
2024-09-24 11:49:11 -0400

    Add many contacts

--- database.py
+++ database.py
@@ -6,7 +6,13 @@ conn=sqlite3.connect('contact_manager.db')
 #Create a cursor
 c = conn.cursor()
 
-c.execute("INSERT INTO contacts VALUES ('Alex', 'Luna', '646-270-0006')")
+many_contacts = [
+					('Jenny','Smith','551-555-0124'),
+					('Maria', 'Aragon','551-296-8001'),
+					('Michael', 'Soriano', '347-818-3092'),
+				]
+
+c.executemany("INSERT INTO contacts VALUES (?,?,?)",many_contacts)
 
 
 print("Command executed successfully")
