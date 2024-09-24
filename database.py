cf069e745f132e53b0da3521ae782afd03c2bc16
nsoler96 <niobisoler96@gmail.com>
2024-09-24 11:36:20 -0400

    Add second contact

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ conn=sqlite3.connect('contact_manager.db')
 #Create a cursor
 c = conn.cursor()
 
-c.execute("INSERT INTO contacts VALUES ('Niobi', 'Soler', '201-931-3469')")
+c.execute("INSERT INTO contacts VALUES ('Elin', 'Moya', '862-318-0388')")
 
 
 print("Command executed successfully")
