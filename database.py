d51f406680c0e72f1d256d69c2beecedce0f5782
nsoler96 <niobisoler96@gmail.com>
2024-09-24 11:27:10 -0400

    Create a table

--- database.py
+++ database.py
@@ -1,4 +1,22 @@
 import sqlite3
 
+#Connect to database
 conn=sqlite3.connect('contact_manager.db')
 
+#Create a cursor
+c = conn.cursor()
+
+#Create a Table
+c.execute("""CREATE TABLE contacts (	
+	first_name text,
+	last_name text,
+	phone_number text 
+	)""")
+
+print("Command executed successully...")
+
+# Commit our command
+conn.commit()
+
+#Close our connection
+conn.close()
